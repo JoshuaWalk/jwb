67f5addc1368cd85ddac6a93e33971a164920f89
Joshua Walker <44381913+JoshuaWalk@users.noreply.github.com>
2021-01-18 14:56:10 -0700

    Add files via upload

--- assignments/user_input.py
+++ assignments/user_input.py
@@ -1,42 +1,44 @@
 from datetime import datetime, timedelta
-# ask for birthdate and validate
-    # date can't be in the future
+
 present = datetime.now()
 
 while True:
+# GET INPUT FROM USER
     try:
       bd = input("input birthday(YYYY-MM-DD)")
       bdate = datetime.strptime(bd, '%Y-%m-%d')
+# VALIDATION
+      if bdate > present:
+          print("use a date from the past")
+          break
     
     except ValueError:
-        print("please use the format: YYYY-MM-DD")     
+        print("please use the format: YYYY-MM-DD")
+# AGE CALCULATION  
     else:
-        break
+        # VARIABLES
+        time_passed = present - bdate
+        days = time_passed.days
+        minutes = days * 24 * 60 
+        seconds = minutes * 60
+        weeks = days / 7
+        months = days / 30.5
 
-if bdate > present:
-            print("You were not born in the future")
+        # USER INPUT
+        age_type = input(""" type the following to see your age in: "months" "weeks" "days" "minutes" or "seconds" """).lower()
 
-# user chooses to see age in months, weeks, days, minutes, or seconds
-    # returns correct age
-
-time_passed = present - bdate
-
-age_type = input(""" type the following to see your age in: "months" "weeks" "days" "minutes" or "seconds" """)
-
-days = time_passed.days
-minutes = days * 24 * 60 
-seconds = minutes * 60
-weeks = days / 7
-months = days / 30.5
-
-print("D: ", days, "W: ", weeks, "M: ", months, "m", minutes, "s", seconds)
-def age_out(age_type):
-        switch={
+        # INPUT FUNCTION/VALIDATION
+        def age_out(age_type):
+            switch={
             "months": f'you are {months} months old',
             "weeks": f'you are {weeks} weeks old',
             "days": f'you are {days} days old',
             "minutes": f'you are {minutes} minutes old',
             "seconds": f'you are {seconds} seconds old'
-        }
-        print(switch.get(age_type, "invalid answer"))
-age_out(age_type)
+            }
+            print(switch.get(age_type, "invalid answer"))
+        # FUNCTION CALL / END OF LOOP
+        age_out(age_type)
+        break
+
+
