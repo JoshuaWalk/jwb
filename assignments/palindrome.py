ae98bffed4f307192ba64adeef6cd12d8ad4be46
Joshua Walker <44381913+JoshuaWalk@users.noreply.github.com>
2021-01-22 15:10:14 -0700

    assignment #2 completed

---
+++ assignments/palindrome.py
@@ -0,0 +1,10 @@
+word = input("Enter a word or phrase to see if it's a palindrome: ")
+clean = ''
+for c in word:
+    if c.isalnum():
+        clean = clean + c.lower()
+reverse = clean[::-1]
+if clean == reverse:
+    print(True) 
+else:
+    print(False)
