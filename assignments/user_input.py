16371dabfe0d4222a579d784e418baf232c10c49
JoshuaWalk <joshuassignup@gmail.com>
2021-01-25 11:10:49 -0700

    finished

--- assignments/user_input.py
+++ assignments/user_input.py
@@ -1,44 +1,44 @@
 from datetime import datetime, timedelta
-
+import math
 present = datetime.now()
 
 while True:
-# GET INPUT FROM USER
     try:
-      bd = input("input birthday(YYYY-MM-DD)")
-      bdate = datetime.strptime(bd, '%Y-%m-%d')
-# VALIDATION
+      bd = input("input birthdate (YYYY-MM-DD) \ntype 'stop' to close\n")
+      birthday = bd[:4] + '-' + bd[5:7] + '-' + bd[8:]
+      if bd == "stop":
+          break
+      bdate = datetime.strptime(birthday, '%Y-%m-%d')
       if bdate > present:
           print("use a date from the past")
-          break
-    
+          continue    
     except ValueError:
         print("please use the format: YYYY-MM-DD")
-# AGE CALCULATION  
-    else:
-        # VARIABLES
-        time_passed = present - bdate
-        days = time_passed.days
-        minutes = days * 24 * 60 
-        seconds = minutes * 60
-        weeks = days / 7
-        months = days / 30.5
 
-        # USER INPUT
-        age_type = input(""" type the following to see your age in: "months" "weeks" "days" "minutes" or "seconds" """).lower()
-
-        # INPUT FUNCTION/VALIDATION
-        def age_out(age_type):
-            switch={
-            "months": f'you are {months} months old',
-            "weeks": f'you are {weeks} weeks old',
-            "days": f'you are {days} days old',
-            "minutes": f'you are {minutes} minutes old',
-            "seconds": f'you are {seconds} seconds old'
-            }
-            print(switch.get(age_type, "invalid answer"))
-        # FUNCTION CALL / END OF LOOP
-        age_out(age_type)
-        break
+    while True:
+        try:
+            age_unit = input("Would you like to see your age in...\nmonths, weeks, days, minutes, seconds \ntype 'stop' to exit\n").lower()
+            time_passed = present - bdate
+            days = time_passed.days
+            minutes = math.floor(days * 24 * 60)
+            seconds = math.floor(days * 24 *60 * 60)
+            weeks = math.floor(days / 7.00)
+            months = math.floor(days / 30.50)
+            if age_unit == "stop":
+                break
+            def age_out(age_unit):
+                switch={
+                "months": f'you are {months} months old\n',
+                "weeks": f'you are {weeks} weeks old\n',
+                "days": f'you are {days} days old\n',
+                "minutes": f'you are {minutes} minutes old\n',
+                "seconds": f'you are {seconds} seconds old\n'
+                }
+                print(switch.get(age_unit, "command not recognized"))
+            age_out(age_unit)
+            continue
+        except ValueError: 
+            print("invalid entry") 
+    break
 
 
