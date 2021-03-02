57f29fca2fbc6c18da27b2d41e743751a11708a0
JoshuaWalk <joshuassignup@gmail.com>
2021-03-02 13:34:00 -0700

    files deleted, added docstrings

--- assignments/age_calculator/agecalulator_test.py
+++
@@ -1,4 +0,0 @@
-import agecalculator, pytest
-
-def test_age():
-    assert agecalculator.getAge('1997-04-08') ==
--- assignments/blackjack_game/consolegame.py
+++
@@ -1,7 +0,0 @@
-import blackjack, blackjack_dealer, blackjack_player
-
-name = input('Enter your name to play blackjack:\n')
-player = blackjack_player.Player(name)
-game = blackjack.Blackjack(player)
-game.playGame()
-
